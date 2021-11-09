b6aa6e1a827a5ee6c0c18817996634e3f2d6eba2
joeprogn1 <joe.1498@hotmail.com>
2021-11-09 01:54:58 -0700

    inicio seccion 3

--- app/src/main/java/com/equipo3/radingup/LoginActivity.java
+++ app/src/main/java/com/equipo3/radingup/LoginActivity.java
@@ -1,14 +1,75 @@
 package com.equipo3.radingup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText mTextInputEmail;
+    TextInputEditText getmTextInputPassword;
+    Button mButtonLogin;
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        getmTextInputPassword  = findViewById(R.id.textInputPassword);
+        mButtonLogin  = findViewById(R.id.btnGoToLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+
+        });
+    }
+    private void login() {
+    String email = mTextInputEmail.getText().toString();
+    String password = getmTextInputPassword.getText().toString();
+
+    if(!email.isEmpty() && !password.isEmpty()){
+        if(password.length() >= 6){
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isComplete()){
+                        Toast.makeText( LoginActivity.this, "El inicio de seion fue exitoso", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText( LoginActivity.this, "La Contraseña o el Correo son incorrecotos", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
+        else {
+            Toast.makeText( LoginActivity.this, "La Contraseña de tener mas 6 caracteres", Toast.LENGTH_SHORT).show();
+        }
+    }
+    else {
+        Toast.makeText( LoginActivity.this, "La Contraseña o el Correo son obligatorios", Toast.LENGTH_SHORT).show();
+    }
     }
 }
--- app/src/main/java/com/equipo3/radingup/SelectOptionAuthActivity.java
+++ app/src/main/java/com/equipo3/radingup/SelectOptionAuthActivity.java
@@ -2,13 +2,38 @@ package com.equipo3.radingup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mButtonGoToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        /*setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("seleccionar opcion");
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    */
+    mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+    mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            goToLogin();
+        }
+
+
+    });
+    }
+    private void goToLogin() {
+        Intent intent = new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
