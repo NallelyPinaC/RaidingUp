84a04f3ca2c07bdb8322ddde7443b7c0cf74b5aa
GenomaVoid <50181252+GenomaVoid@users.noreply.github.com>
2021-11-10 19:02:39 -0700

    S3-C14-ED
    video numero 4 y 14 de firebase funcionando

--- app/src/main/java/com/equipo3/radingup/SelectOptionAuthActivity.java
+++ app/src/main/java/com/equipo3/radingup/SelectOptionAuthActivity.java
@@ -1,12 +1,12 @@
 package com.equipo3.radingup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toolbar;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
@@ -17,20 +17,20 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
-        //mToolbar = findViewById(R.id.toolbar);
-        /*setSupportActionBar(mToolbar);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("seleccionar opcion");
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
-    */
-    mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
-    mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            goToLogin();
-        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
 
 
-    });
+        });
     }
     private void goToLogin() {
         Intent intent = new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
